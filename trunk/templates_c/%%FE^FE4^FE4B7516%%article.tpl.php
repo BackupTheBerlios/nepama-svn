aaf041a5e225a8cbd1a7ea20c93fa35af7894ebb
neinandy <neinandy@6f72c2d0-effe-0310-b8d7-98e519aed7ca>
2006-02-26 18:08:58 +0000

    Removed file/folder

--- trunk/templates_c/%%FE^FE4^FE4B7516%%article.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.11, created on 2006-02-18 18:45:46
-         compiled from default/modules/article/article.tpl */ ?>
-<h1>&rarr; Artikel: <?php echo $this->_tpl_vars['title']; ?>
-</h1>
-
-<?php echo $this->_tpl_vars['content']; ?>
